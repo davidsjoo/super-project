ff52183755aed7cd28f02c9303aa8f3bedc5c8af
davidsjoo <davidsjoo@gmail.com>
2018-03-10 23:03:52 +0100

    Config

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
 	console.log('Test');
@@ -9,9 +11,31 @@ gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('watch', function(){
+gulp.task('watch',['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
   // Other watchers
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 })
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('styles', function(){
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    //.pipe(prefix('last 2 versions'))
+    .pipe(concat('main.css'))
+    //.pipe(minifyCSS())
+    .pipe(gulp.dest('app/css'))
+});
